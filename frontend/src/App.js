444f971cb2cc60aa71299c38958d2a9771be9f90
Udayasooriyan Prabudeva <prabud0401@gmail.com>
2024-04-17 10:03:28 +0530

    updated app.js page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter , Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./auth/login";
 import Signup from "./auth/signup";
 import Admin from "./admin/admin";
@@ -7,23 +7,34 @@ import Home from "./home/home";
 import AddTrain from "./admin/addTrain";
 import UpdateTrain from "./admin/UpdateTrain";
 import Booking from "./booking/booking";
+import './css/body.css';
+import './css/login.css';
 
-
+import trainLogo from "./assets/train-logo.png"; 
 function App() {
+  const handleClick = () => {
+    window.location.href = "/home";
+  };
+
   return (
-    <BrowserRouter>
+    <Router>
+      <header>
+        <button onClick={handleClick} className="logo">
+          <img src={trainLogo} alt="Train Logo" className="logo" />
+        </button>
+      </header>
       <div>
         <Routes>
-          <Route path="/login" element={<Login />} ></Route>
-          <Route path="/signup" element={<Signup />} ></Route>
-          <Route path="/home" element={<Home />} ></Route>
-          <Route path="/admin" element={<Admin />} ></Route>
-          <Route path="/addTrain" element={<AddTrain />} ></Route>
-          <Route path="/updateTrain" element={<UpdateTrain />} ></Route>
-          <Route path="/booking" element={<Booking />} ></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/addTrain" element={<AddTrain />} />
+          <Route path="/updateTrain" element={<UpdateTrain />} />
+          <Route path="/booking" element={<Booking />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
